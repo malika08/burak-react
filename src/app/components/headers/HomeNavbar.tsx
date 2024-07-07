62faecd2215991914fef2a6cfa4d1af6ecbd39f2
malika_south.k <malikaatoeva96@gmail.com>
2024-07-07 12:54:35 +0900

    fix:modify head components

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -5,27 +5,18 @@ export function HomeNavbar() {
   const authMember = null;
   return (
     <div className="home-navbar">
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{ height: "50px" }}
-          flexDirection={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+      <Container className="navbar-container">
+        <Stack className="menu">
           <Box>
             <NavLink to="/">
               <img
+                className="brand-logo"
                 src="/icons/burak.svg"
                 style={{ width: "125px", height: "30px" }}
               />
             </NavLink>
           </Box>
-          <Stack
-            flexDirection={"row"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            minWidth={"700px"}
-          >
+          <Stack className="links">
             <Box className={"hover-line"}>
               <NavLink
                 to="/"
@@ -73,19 +64,38 @@ export function HomeNavbar() {
             {/** Basket */}
             {!authMember ? (
               <Box className={"hover-line"}>
-                <Button
-                  variant="contained"
-                  style={{ background: "#3776CC", color: "#f8f8ff" }}
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
             ) : (
-              <img />
+              <img
+                className="user-avatar"
+                src="/icons/default-user.svg"
+                aria-haspopup={"true"}
+              />
             )}
           </Stack>
         </Stack>
-        <Stack>Detail</Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-text"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"welcome-text"}>The Choice, not just a Choice</Box>
+            <Box className={"service-text"}> 24 hours service</Box>
+            <Box className={"signup"}>
+              {!authMember ? (
+                <Button variant={"contained"} className="signup-button">
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+          </Box>
+        </Stack>
       </Container>
     </div>
   );
