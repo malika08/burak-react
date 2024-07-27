5abf5252e2072f5854aa22feaea8057b4774d1bb
malika_south.k <malikaatoeva96@gmail.com>
2024-07-28 06:36:08 +0900

    feat: create HomePage Redux configurations

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -6,7 +6,15 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import "../../../css/home.css";
 import Events from "./Events";
+
 export default function HomePage() {
+  // Selector: Store => data
+
+  useEffect(() => {
+    //backend server data request => Data
+    //slice: Data => Store
+  }, []);
+
   return (
     <div className={"homepage"}>
       <Statistics />
